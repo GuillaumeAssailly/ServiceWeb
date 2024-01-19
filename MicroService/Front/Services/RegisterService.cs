3955d4435135fe54c0b33c015574b28c16fed37f
MuraAntoine <arum.antoine@gmail.com>
2024-01-19 14:15:39 +0100

    Ajout message d'erreur connexion

--- MicroService/Front/Services/RegisterService.cs
+++ MicroService/Front/Services/RegisterService.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging; // Ajoutez cette ligne pour utiliser ILogger
 using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Security.Claims;
@@ -12,16 +13,16 @@ namespace Front.Services
     public class RegisterService
     {
         private readonly HttpClient _httpClient;
+        private readonly ILogger<RegisterService> _logger; // Ajoutez cette ligne
 
-        public RegisterService(HttpClient httpClient)
+        public RegisterService(HttpClient httpClient, ILogger<RegisterService> logger) // Ajoutez ILogger dans le constructeur
         {
             _httpClient = httpClient;
+            _logger = logger; // Ajoutez cette ligne
             _httpClient.BaseAddress = new System.Uri("http://localhost:5000");
         }
-
         public async Task<UserDTO> RegisterUser(string username, string password, string mail)
         {
-
             UserCreateModel user = new UserCreateModel()
             {
                 Name = username,
@@ -30,36 +31,32 @@ namespace Front.Services
             };
 
             var response = await _httpClient.PostAsJsonAsync("/api/User/register", user);
-            
-            if(response.StatusCode == HttpStatusCode.OK)
+
+            if (response.StatusCode == HttpStatusCode.OK)
             {
                 var result = await response.Content.ReadFromJsonAsync<UserDTO>();
                 return result;
-            } else if (response.StatusCode == HttpStatusCode.Conflict)
+            }
+            else if (response.StatusCode == HttpStatusCode.Conflict)
             {
                 var errorMessage = await response.Content.ReadAsStringAsync();
 
-
-
-
-
-
                 if (errorMessage.Contains("Email"))
                 {
-                    Console.WriteLine("Email already exists");
+                    _logger.LogInformation("Email already exists");
+
                 }
                 else if (errorMessage.Contains("Username"))
                 {
-
-                    Console.WriteLine("Username already exists");
+                    _logger.LogInformation("Username already exists");
                 }
                 else
                 {
-                    Console.WriteLine("Registration failed due to conflict: " + errorMessage);
+                    _logger.LogError("Registration failed due to conflict: " + errorMessage);
                 }
             }
+
             return null;
-            
         }
     }
 }
